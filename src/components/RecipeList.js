f5cac61087bd0f7426407e1bb8316dfdbb643776
Eirik <eirikag1994@gmaail.com>
2020-10-11 22:34:04 +0200

    Search box

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -6,13 +6,11 @@ import Col from "react-bootstrap/Col";
 import axios from "axios";
 import RecipeItem from './RecipeItem';
 import Search from "./Search";
-
 function RecipeList() {
   // keep all the recipies in the "store"
   const [recipes, setRecipies] = useState([])
   const [filteredRecipes, setFilteredRecipes] = useState([]);
   const [loading, setLoading] = useState(true);
-
 
   // fetch all the recipies from the backend
   // we are using a proxy since the backend is not CORS enabled
@@ -20,61 +18,41 @@ function RecipeList() {
     axios.get("https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/").then(response => {
       console.log(response.data.results)
       setRecipies(response.data.results)
+      setFilteredRecipes(response.data.results)
     }).catch(error => {
       console.log(error)
-      .finally(() => setLoading(false));
-    })
+    }).finally(() => setLoading(false));
   };
-
   // get all the recipies when this component loads
   useEffect(() => {
     getRecipies()
   }, [])
-
-  const filterRecipes = function(e) {
+  const filterRecipes = function (e) {
     const searchValue = e.target.value.toLowerCase();
-
-    const filteredArray = recipes.filter(function(recipe) {
-        const lowerCaseName = recipe.name.toLowerCase();
-
-        if (lowerCaseName.startsWith(searchValue)) {
-            return true;
-        }
-        return false;
+    const filteredArray = recipes.filter(function (recipe) {
+      const lowerCaseName = recipe.title.toLowerCase();
+      if (lowerCaseName.includes(searchValue)) {
+        return true;
+      }
+      return false;
     });
-
     // set filtered recipes to the new array
     setFilteredRecipes(filteredArray);
   };
 
-
   if (loading) {
     return <Spinner animation="border" className="spinner" />;
   }
-
   return (
     <div>
       <h1>Recipies</h1>
-
-      {recipes.map((recipe) =>
+      <Search handleSearch={filterRecipes} />
+      {filteredRecipes.map((recipe) =>
         <div>
           <RecipeItem title={recipe.title} ingredients={recipe.ingredients} href={recipe.href} thumbnail={recipe.thumbnail} />
-          <Search handleSearch={filterRecipes} />
-          <RecipeList handleSearch={filterRecipes} />
-          <Row>
-              {filteredRecipes.map(recipe => {
-                 const { id, name, image } = recipe;
-                    <Col sm={6} md={3} key={id}>;
-                       <RecipeList id={id} name={name} image={image} />
-                    </Col>   
-              }
-              )
-            }
-          </Row>
-        )
         </div>
       )}
-   </div>
+    </div>
   )
-  }
+}
 export default RecipeList;
